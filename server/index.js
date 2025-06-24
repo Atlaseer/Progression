e896772780be7c1191a0babc0dee49e2ec0c10d6
“Simon <atlaspersson@outlook.com>
2025-06-24 22:37:44 +0200

    installed more dependencies

--- server/index.js
+++ server/index.js
@@ -56,5 +56,5 @@ mongoose.connect(process.env.MONGO_URI)
         })
     })
     .catch(err =>{
-        console.error('Failed to conenct to MongoDB', err)
+        console.error('Failed to connect to MongoDB', err)
     })
