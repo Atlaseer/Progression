08b2e175c756531f945266c7457535549bab0639
“Simon <atlaspersson@outlook.com>
2025-06-24 13:52:34 +0200

    Updated /server/index.js

--- server/index.js
+++ server/index.js
@@ -3,3 +3,17 @@ import mongoose from 'mongoose';
 import cors from 'cors';
 import path from 'path';
 import dotenv from 'dotenv';
+import ip from 'ip';
+import cookieParser from 'cookie-parser';
+import { fileURLToPath } from 'url';
+
+dotenv.config();
+const app = express();
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+app.use('/uploads', express.static(path.join(__dirname, 'uploads')))
+
+const PORT = process.env.PORT || 5000
+
+const allowedOrigins = ['http://localhost', 'http://localhost:5173', 'http://localhost:3000', 'https://']
