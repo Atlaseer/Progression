f0ca6e880acf4db8b0e1769bc64b0b63bad90172
“Simon <atlaspersson@outlook.com>
2025-07-03 09:33:59 +0200

    Finished POST and GET all exercises in eercise.js

--- server/routes/exercise.js
+++ server/routes/exercise.js
@@ -6,12 +6,40 @@ const router = express.Router();
 //Create exercise
 router.post('/', async (req, res) => {
   try {
-    const newExercise = new Exercise(req.body);
+    const { name, muscleGroup, equipment, description, pictureUrl } = (req.body);
+
+    if (!name || !muscleGroup) return res.status(400).json({ error: "Name and musclegroup are required" });
+
+    const newExercise = new Exercise({
+      name, 
+      muscleGroup, 
+      equipment, 
+      description, 
+      pictureUrl
+    })
+
     await newExercise.save();
     res.status(201).json({ message: 'Exercise created!', exercise: newExercise });
+
   } catch (err) {
-    res.status(400).json({ error: err.message });
+    res.status(500).json({ error: 'Failed to create exercise' });
   }
 });
+
+//Get all exercises
+router.get('/', async (req, res) => {
+  try {
+    const exercises = await Exercise.find();
+    res.json(exercises);
+
+  } catch (err) {
+    res.status(500).json({ error: 'Failed to fetch exercises' })
+  }
+})
+
+
+router.delete('/', async (req, res) => {
+
+})
 
 export default router;
